ea041b15bf1b8eb82370fd5e9acc5956201fac62
Dan Perrera <dan@perrera.com>
2016-12-06 17:50:27 -0500

    more reliable jekyll syncing

--- tasks/gulp/default.js
+++ tasks/gulp/default.js
@@ -5,7 +5,8 @@ const path = require('path')
 gulp.task('watch', () => {
   gulp.watch(path.join(config.shuboxWeb.styles.src, '**'), ['sass'])
   gulp.watch(path.join(config.shuboxWeb.scripts.src, '**'), ['js'])
-  gulp.watch(path.join(config.shuboxWeb.jekyll.src, '**'), ['jekyll-build', 'jekyll-sync'])
+  gulp.watch(path.join(config.shuboxWeb.jekyll.src, '**'), ['jekyll-build'])
+  gulp.watch(path.join(config.shuboxWeb.jekyll.tmp, '**'), ['jekyll-sync'])
 })
 
 gulp.task('default', ['browserSync', 'watch'])
