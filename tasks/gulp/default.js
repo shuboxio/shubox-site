152352884d6ec332aaf775af1cc981788002416e
Dan Perrera <dan@perrera.com>
2016-12-16 17:11:18 -0500

    fresh homepage content

--- tasks/gulp/default.js
+++ tasks/gulp/default.js
@@ -3,8 +3,8 @@ const gulp = require('gulp')
 const path = require('path')
 
 gulp.task('watch', () => {
-  gulp.watch(path.join(config.shuboxWeb.styles.src, '**'), ['sass'])
   gulp.watch(path.join(config.shuboxWeb.scripts.src, '**'), ['js'])
+  gulp.watch(path.join(config.shuboxWeb.styles.src, '**'), ['sass'])
   gulp.watch(path.join(config.shuboxWeb.jekyll.src, '**'), ['jekyll-build'])
   gulp.watch(path.join(config.shuboxWeb.jekyll.tmp, '**'), ['jekyll-sync'])
 })
