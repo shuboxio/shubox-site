b743ac73770be96a1031c989a9aedff78edf5afa
Dan Perrera <dan@foredesign.co>
2017-01-24 10:35:03 -0500

    better type styles and one sticky navigation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,3 +3,21 @@
 $('#menu-toggle').on('click', function () {
   $('.main-nav').slideToggle()
 })
+
+// ============================================================
+// Sticky Subnav
+// ============================================================
+
+function sticky (el) {
+  var elem = document.querySelector(el)
+  var sticky = document.getElementById('sticky')
+  if (elem.offsetTop <= window.scrollY) {
+    sticky.classList.add('-stuck')
+  } else {
+    sticky.classList.remove('-stuck')
+  }
+}
+
+window.addEventListener('scroll', function (e) {
+  sticky('.layout-subnav')
+})
