35dcad95fa879fe5360c807411d876ff1ee408e9
Joel Oliveira <joel.oliveira@gmail.com>
2017-03-30 10:35:32 -0400

    Add deploy task.
    Needed in order to work: ENV variables for aws key, secret, and bucket.

---
+++ tasks/gulp/deploy.js
@@ -0,0 +1,16 @@
+var config = require('../../config')
+var gulp = require('gulp')
+var s3 = require('gulp-s3-upload')({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY
+})
+
+gulp.task('deploy', () => {
+  return gulp.src(config.shuboxWeb.jekyll.dest)
+    .pipe(s3({
+      Bucket: process.env.S3_DEPLOY_BUCKET,
+      ACL: 'public-read'
+    }, {
+      maxRetries: 5
+    }))
+})
