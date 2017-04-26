c056f03135de19706264b5c50df909f1b0ee40d7
Joel Oliveira <joel.oliveira@gmail.com>
2017-04-26 16:15:40 -0400

    Remove task(s) depending on the s3 upload plugin

--- tasks/gulp/default.js
+++ tasks/gulp/default.js
@@ -8,12 +8,6 @@ gulp.task('watch', () => {
   gulp.watch(path.join(config.shuboxWeb.styles.src, '**'), ['sass'])
   gulp.watch(path.join(config.shuboxWeb.jekyll.src, '**'), ['jekyll-build'])
   gulp.watch(path.join(config.shuboxWeb.jekyll.tmp, '**'), ['jekyll-sync'])
-
-  if (process.env.S3_CODEPEN_BUCKET &&
-    process.env.AWS_ACCESS_KEY &&
-    process.env.AWS_SECRET_KEY) {
-    gulp.watch(path.join(config.shuboxWeb.styles.dest, './codepen.css'), ['codepen'])
-  }
 })
 
 gulp.task('default', ['browserSync', 'watch'])
