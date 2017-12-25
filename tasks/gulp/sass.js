15ce3d882cb0b3bb05318a38e12b37c0902b718f
Joel Oliveira <joel.oliveira@gmail.com>
2017-12-24 21:26:51 -0500

    Upgrade dependencies

--- tasks/gulp/sass.js
+++ tasks/gulp/sass.js
@@ -1,25 +1,31 @@
-var autoprefixer = require('autoprefixer')
-var browserSync = require('browser-sync')
-var config = require('../../config')
-var gulp = require('gulp')
-var notify = require('gulp-notify')
-var path = require('path')
-var postcss = require('gulp-postcss')
-var sass = require('gulp-sass')
-var sourcemaps = require('gulp-sourcemaps')
+var autoprefixer = require('autoprefixer');
+var browserSync = require('browser-sync');
+var config = require('../../config');
+var gulp = require('gulp');
+var notify = require('gulp-notify');
+var path = require('path');
+var postcss = require('gulp-postcss');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var stripCssComments = require('gulp-strip-css-comments');
 
 gulp.task('sass', () => {
-  return gulp.src(path.join(config.shuboxWeb.styles.src, '*.sass'))
+  return gulp
+    .src(path.join(config.shuboxWeb.styles.src, '*.sass'))
     .pipe(sourcemaps.init())
-    .pipe(sass({
-      errLogToConsole: true,
-      indentedSyntax: true,
-      outputStyle: 'compressed'
-    })).on('error', function (err) { return notify().write(err) })
-    .pipe(postcss([
-      autoprefixer({ browsers: ['last 2 versions'] })
-    ]))
+    .pipe(
+      sass({
+        errLogToConsole: true,
+        indentedSyntax: true,
+        outputStyle: 'compressed',
+      }),
+    )
+    .on('error', function(err) {
+      return notify().write(err);
+    })
+    .pipe(postcss([autoprefixer({browsers: ['last 2 versions']})]))
     .pipe(sourcemaps.write('.'))
+    .pipe(stripCssComments({preserve: false}))
     .pipe(gulp.dest(config.shuboxWeb.styles.dest))
-    .pipe(browserSync.stream({match: '**/*.css'}))
-})
+    .pipe(browserSync.stream({match: '**/*.css'}));
+});
