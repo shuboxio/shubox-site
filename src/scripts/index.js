a5bda29f9d4534c9f9dbc597fd0cf3aa7651b832
Joel Oliveira <joel.oliveira@gmail.com>
2022-10-20 20:51:57 -0400

    Remove mixpanel.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,20 +1,5 @@
 /* global $, hljs */
 
-window.addEventListener("load", function () {
-  function newsletterTrack () {
-    var subscribeForm = document.getElementById('mc-embedded-subscribe-form')
-    subscribeForm.addEventListener("submit", function(e){
-      var email = e.target.querySelector("input.email").value
-
-      if (email !== "") {
-        mixpanel.track("Subscribed to Newsletter", { email: email })
-      }
-    })
-  }
-
-  newsletterTrack()
-});
-
 var scrolled = false;
 
 window.addEventListener('scroll', function(e) {
