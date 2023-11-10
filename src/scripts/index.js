15a13ec1f490e88fbfa1994d14052704576ecb78
Joel Oliveira <joel.oliveira@gmail.com>
2023-11-10 16:15:04 -0500

    Updates to the homepage. Copy + navigation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,16 +2,29 @@
 
 var scrolled = false;
 
-window.addEventListener('scroll', function(e) {
-  var smallChatId = document.body.dataset.smallChatId;
-  if(scrolled || smallChatId == "123") { return }
-
+var chatLoader = function() {
   var script = document.createElement("script")
   script.src = "https://embed.small.chat/"+ smallChatId +".js"
   document.body.appendChild(script);
 
   // force smallchat to load
   setTimeout(function(){ dispatchEvent(new Event('load')) }, 500)
+}
 
+var scrollHandler = function(e) {
+  var nav = document.querySelector('.global-header');
+
+  if (window.scrollY > 200) {
+    nav.attributes.class.value = nav.attributes.class.value.replace('pv4', 'pv3');
+  } else {
+    nav.attributes.class.value = nav.attributes.class.value.replace('pv3', 'pv4');
+  }
+
+  var smallChatId = document.body.dataset.smallChatId;
+  if(scrolled || smallChatId == "123") { return }
+
+  chatLoader();
   scrolled = true;
-});
+}
+
+window.addEventListener('scroll', scrollHandler);
--- src/scripts/stickynav.js
+++ src/scripts/stickynav.js
@@ -1,7 +1,9 @@
 window.addEventListener("load", function(){
+  debugger
   var threshold = document.querySelector('.threshold');
   var nav = document.querySelector('.subnav nav');
   var handler = function(entries) {
+    debugger
     if (!entries[0].isIntersecting) {
       nav.classList.add('stuck')
     } else {
