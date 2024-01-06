7f9c94d6687236bcadb2883ddf08ee00fc4c49a4
Joel Oliveira <joel.oliveira@gmail.com>
2024-01-06 12:16:00 -0500

    Homepage mobile-first styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: ["./_site/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'none': 'none',
+      },
+      colors: {
+        'shubox-purple': '#bf9de0',
+        'shubox-dark-purple': '#7c5cd1',
+        'shubox-light-purple': '#f9f2ff',
+        'shubox-red': '#f56565',
+        'shubox-gray': '#777777',
+        'shubox-silver': '#999',
+        'shubox-near-white': '#faf9f8',
+        'shubox-light-gray': '#edeae6',
+        'shubox-dark-gray': '#3f3f40',
+        'shubox-charcoal': '#3f3f40',
+      },
+    },
   },
   plugins: [],
 }
